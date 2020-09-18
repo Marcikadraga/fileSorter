adf7474de0f4828dd22142af0f8783f663405b1d
Marci <marciprog@gmail.com>
2020-09-18 14:16:59 +0200

    adding files

---
+++ FileSorter1.5/Init.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileSorter
+{
+    public class Init
+    {
+        public string DestDirPath { get; set; }
+        public string StartDiryPath { get; set; }
+        public string[] Files { get; set; }
+        public Init()
+        {
+            StartDiryPath = @"C:\Users\Balogh Márton\Desktop\folder1\";
+            DestDirPath = @"C:\Users\Balogh Márton\Desktop\folder2\";
+            Files = Directory.GetFileSystemEntries(StartDiryPath);
+        }
+        public void SortTheFolder()
+        {
+            foreach (var file in Files)
+            {
+                string ext = Path.GetExtension(file);
+                if (Directory.Exists(file))
+                {
+                    SuperMoveToNewDirectory(startPath: file, destinationPath: DestDirPath, filesDateOfCreation: "", extension: "", foldersName: "folders", item: file);
+                }
+                else if (File.Exists(file))
+                {
+                    if (ext == ".avi" || ext == ".mp4")
+                    {
+                        SuperMoveToNewDirectory(startPath: file, destinationPath: DestDirPath, filesDateOfCreation: File.GetCreationTime(file).ToString().Replace(":", " "), extension: "", foldersName: "Movies", item: file);
+                    }
+                    else
+                    {
+                        SuperMoveToNewDirectory(startPath: file, destinationPath: DestDirPath, filesDateOfCreation: "", extension: ext, foldersName: "", item: file);
+                    }
+                }
+            }
+        }
+        public void SuperMoveToNewDirectory(string startPath, string destinationPath, string extension, string foldersName, string filesDateOfCreation, string item)
+        {
+            Directory.CreateDirectory($"{destinationPath}{extension}{foldersName}");
+            if (Directory.Exists(item))
+            {
+                Directory.Move(item, $@"{DestDirPath}{foldersName}\{new DirectoryInfo(item).Name}");
+            }
+            if (File.Exists(item))
+            {
+                File.Move(startPath, $@"{destinationPath}{foldersName}\{filesDateOfCreation}{extension}\{extension}");
+            }
+        }
+    }
+}
---
+++ FileSorter1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using FileSorter;
+using System;
+
+namespace FileSorter1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init i = new Init();
+            i.SortTheFolder();
+        }
+    }
+}
